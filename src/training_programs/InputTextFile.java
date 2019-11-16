c003ac99bf77e553d2c33c23057f0b1ff0cc1791
VadMih <mirent@mail.ru>
2019-11-16 22:00:46 +0300

    Read InputTextFile.java

--- src/training_programs/InputTextFile.java
+++ src/training_programs/InputTextFile.java
@@ -1,10 +1,38 @@
 package training_programs;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class InputTextFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader br = null;
+		try {
+			// Определение объекта для чтения файла
+			br = new BufferedReader(new FileReader("history_command.txt"));
+			// Переменная для считывания данных из файла
+			String line;
+			// Процесс считывания из файла
+			while((line = br.readLine()) != null)
+			{
+				System.out.println(line);
+			}
+		} catch(IOException e) {
+			System.out.print("Error " + e);
+		}
+		finally
+		{
+			try
+			{
+				br.close();
+			} catch(IOException e)
+			{
+				System.out.print("Error " + e);
+			}
+		}
 	}
 
 }
