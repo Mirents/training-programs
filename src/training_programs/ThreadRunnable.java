71b282972887ec83222aac9b1b09542b738b65d5
VadMih <mirent@mail.ru>
2019-11-19 20:02:28 +0300

    Read ThreadRunnable.java

--- src/training_programs/ThreadRunnable.java
+++ src/training_programs/ThreadRunnable.java
@@ -1,10 +1,32 @@
 package training_programs;
 
+class someThtead extends Thread
+{
+	public void run() {
+		for(int i = 1; i <= 100; i++)
+		{
+			System.out.println("Thread number " + i);
+		}
+	}
+}
+
+class someRunnable implements Runnable
+{
+
+	@Override
+	public void run() {
+		for(int i = 1; i <= 100; i++)
+		{
+			System.out.println("Runnable number " + i);
+		}
+	}
+	
+}
+
 public class ThreadRunnable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
