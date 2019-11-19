02c5f6083d5559da882261b508c468762f4cdf28
VadMih <mirent@mail.ru>
2019-11-19 21:16:07 +0300

    Read CaesarCilper.java

--- src/training_programs/CaesarCipher.java
+++ src/training_programs/CaesarCipher.java
@@ -25,6 +25,36 @@ public class CaesarCipher {
 	// Метод перестановки букв в строке
 	public static String rev(String s)
 	{
-		
+		// Временный буфер для работы с текстом
+    StringBuilder sb = new StringBuilder(s);
+    // Основной цикл
+    for(int i=0;i<sb.length();i++)
+    {
+        // Первый символ для проверки из текста
+        char c1 = sb.charAt(i);
+        // Дополнительный цикл прохода по алфавиту
+        for(int y=0;y<albh.length();y++)
+        {
+        // Второй символ для проверки из алфавита
+        char c2 = albh.charAt(y);
+        // Если символы равны
+        if(c1==c2)
+        {
+        // Если это не последний элемент алфавита
+        if(y!=albh.length()-1)
+        {
+        // Присваиваем тексту следующее значение
+        sb.setCharAt(i, albh.charAt(y+1));
+        }
+        else
+        {
+        // Если это последний элемент - присваиваем первкю букву алфавита
+        sb.setCharAt(i, albh.charAt(0));
+        }
+        }
+        }
+    }
+    // Возврат измененного текста
+    return sb.toString();
 	}
 }
