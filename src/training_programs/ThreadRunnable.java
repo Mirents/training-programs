ba47ac92994ddabaacc8bf594a3e3b7a939ed250
VadMih <mirent@mail.ru>
2019-11-19 20:05:05 +0300

    Read ThreadRunnable.java

--- src/training_programs/ThreadRunnable.java
+++ src/training_programs/ThreadRunnable.java
@@ -26,7 +26,17 @@ class someRunnable implements Runnable
 public class ThreadRunnable {
 
 	public static void main(String[] args) {
+		// Реализация работы двух потоков первым методом
+		someThtead sT1 = new someThtead();
+		sT1.start();
+		someThtead sT2 = new someThtead();
+		sT2.start();
 		
+		// Реализация работы двух потоков вторым методом
+		Thread sR1 = new Thread(new someRunnable());
+		Thread sR2 = new Thread(new someRunnable());
+		sR1.start();
+		sR2.start();
 	}
 
 }
