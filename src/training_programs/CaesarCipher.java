69df68176d3a07c2827d671e49ec3ce30f9205d2
VadMih <mirent@mail.ru>
2019-11-19 20:32:41 +0300

    Read CaesarCilper.java

--- src/training_programs/CaesarCipher.java
+++ src/training_programs/CaesarCipher.java
@@ -7,10 +7,24 @@ package training_programs;
 Input: "defend the east wall of the castle"
 Output: "efgfoe uif fbtu xbmm pg uif dbtumf"*/
 
+import java.util.Scanner;
+
 public class CaesarCipher {
 
-	public static void main(String[] args) {
+	// Статическая переменная для проверки алфавита
+	public static final String albh = "abcdefghijklmnopqrstuvwxyz";
 
+	public static void main(String[] args) {
+		Scanner myEnt = new Scanner(System.in);
+		String str = "";
+		// Вводим текстовую строку
+		str = myEnt.nextLine();
+		System.out.println(rev(str));
 	}
 
+	// Метод перестановки букв в строке
+	public static String rev(String s)
+	{
+		
+	}
 }
