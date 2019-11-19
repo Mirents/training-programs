f01115d54773e5a93fe2894d5a9c5029016bd3f7
VadMih <mirent@mail.ru>
2019-11-19 20:21:37 +0300

    Read CaesarCilper.java

--- src/training_programs/CaesarCipher.java
+++ src/training_programs/CaesarCipher.java
@@ -1,5 +1,12 @@
 package training_programs;
 
+/*
+Секретный способ цезаря общения с генералами.
+Состоял в том, что он заменял букву на следующую в алфавите.
+Пример:
+Input: "defend the east wall of the castle"
+Output: "efgfoe uif fbtu xbmm pg uif dbtumf"*/
+
 public class CaesarCipher {
 
 	public static void main(String[] args) {
