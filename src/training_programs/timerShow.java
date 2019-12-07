dc5e03042ddb3c28629773544a75e9ad9113a871
Vadim <55352561+VadMih@users.noreply.github.com>
2019-12-07 18:09:42 +0300

    Update timerShow.java

--- src/training_programs/timerShow.java
+++ src/training_programs/timerShow.java
@@ -1,10 +1,46 @@
 package training_programs;
 
-import java.util.Scanner;
-
 public class timerShow {
-
+ 
 	public static void main(String[] args) {
-
+		int thisPeriodTimer = 0; // Текущий период, 0-работа, 1-отдых, 2-большой перерыв
+		int circleWorkTimer = 1; // Текущий цикл работы до большого перерыва
+		int [] schemeTimerMinute = {5, 3, 4, 3}; // Массив переменных - {время работы,
+		// перерыв, большой перерыв, количество циклов до большого перерыва
+		int countTimerSecond = schemeTimerMinute[thisPeriodTimer]; // Счетчик вреиени
+  System.out.println(thisPeriodTimer + " " + circleWorkTimer);
+		while (countTimerSecond > -1) {
+			//countTimerSecond--;
+			//System.out.println(countTimerSecond+" min");
+			if(countTimerSecond<=0) {
+				if(thisPeriodTimer==0) {
+					if(circleWorkTimer==schemeTimerMinute[3]) {
+						circleWorkTimer = 1;
+						thisPeriodTimer = 2;
+						countTimerSecond = schemeTimerMinute[thisPeriodTimer];
+						//System.out.println("Большой перерыв!");
+						System.out.println(thisPeriodTimer + " " + circleWorkTimer);
+					} else {
+						circleWorkTimer++;
+						thisPeriodTimer = 1;
+						countTimerSecond = schemeTimerMinute[thisPeriodTimer];
+						//System.out.println("Начинаем работать!");
+						System.out.println(thisPeriodTimer + " " + circleWorkTimer);
+					}
+				} else if (thisPeriodTimer == 1) {
+					thisPeriodTimer = 0;
+					countTimerSecond = schemeTimerMinute[thisPeriodTimer];
+					//System.out.println("Отдых закончен!");
+					System.out.println(thisPeriodTimer + " " + circleWorkTimer);
+				} else if (thisPeriodTimer == 2) {
+					thisPeriodTimer = 0;
+					// circleWorkTimer = schemeTimerMinute[thisPeriodTimer];
+					circleWorkTimer = 0;
+					//System.out.println("Отдых закончен!");
+					System.out.println(thisPeriodTimer + " " + circleWorkTimer);
+			}
+		}
+		countTimerSecond--;
+		}
 	}
 }
