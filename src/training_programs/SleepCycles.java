e21429d35363e3ab4bc7db5ceea1f42cab9972ad
VadMih <mirent@mail.ru>
2019-12-14 17:03:04 +0300

    Read SleepCycle.java

--- src/training_programs/SleepCycles.java
+++ src/training_programs/SleepCycles.java
@@ -4,33 +4,35 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
 
 public class SleepCycles {
-
-	private static final long serialVersionUID = -8312085209948270707L;
-	private JPanel contentPane;
 	
 	public static void main(String[] args) {
 
 		JFrame jFrame = new JFrame();
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.setSize(400, 100);
+		jFrame.setSize(300, 300);
 		jFrame.setVisible(true);
 		
 		JPanel jPanel = new JPanel();
-		jFrame.add(jPanel);
+		jFrame.getContentPane().add(jPanel);
 		
 		// Менеджер FlowLayout
-		jPanel.setLayout(new FlowLayout());
+		/*jPanel.setLayout(new FlowLayout());
 		jPanel.add(new JButton("Button 1"));
 		JTextField textBox = new JTextField(10);
 		jPanel.add(textBox);
+		JTextPane textPane = new JTextPane();
+		JScrollPane textScroll = new JScrollPane(textPane);
+		textScroll.setBounds(5, 30, 500, 150);
+		textPane.setBounds(5, 30, 500, 150);
+		jPanel.add(textPane);*/
 		
 		// Менеджер BorderLayout
 		/*jPanel.setLayout(new BorderLayout());
@@ -42,5 +44,14 @@ public class SleepCycles {
 		jPanel.add(new JButton("Button 2"));
 		jPanel.add(new JButton("Button 3"));
 		jPanel.add(new JButton("Button 4"));*/
+		
+		jPanel.setLayout(new GridLayout(3,1));
+		jPanel.add(new JButton("Button 1"));
+		JTextField textBox = new JTextField(10);
+		jPanel.add(textBox);
+		JTextPane textPane = new JTextPane();
+		JScrollPane textScroll = new JScrollPane(textPane);
+		textScroll.setBounds(5, 30, 500, 150);
+		jPanel.add(textScroll);
 	}
 }
