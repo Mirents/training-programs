bbcd6b1194ec381bf32fd10a80505452bc5cf527
VadMih <mirent@mail.ru>
2019-12-15 23:56:20 +0300

    Delete packade to file testJava.java

--- src/training_programs/testJava.java
+++ src/training_programs/testJava.java
@@ -1,7 +1,7 @@
-package training_programs;
+//package training_programs;
 
 public class testJava {
   public static void main(String[] args) {
-    System.out.println("testJava");
+    System.out.println("Hello text!!!");
   }
 }
