de252ba7e54951584b1eb302dfddc1498365e4f6
VadMih <mirent@mail.ru>
2019-12-16 17:24:31 +0300

    Read file GuessLaunher.java

--- src/training_programs/GuessLauncher.java
+++ src/training_programs/GuessLauncher.java
@@ -1,3 +1,5 @@
+// Класс для запуска приложения игра угадай число
+// Три объекта класса имитируют игроков, которые угадывают число
 public class GuessLauncher {
   public static void main(String[] args) {
     GuessGame gG = new GuessGame();
@@ -5,32 +7,37 @@ public class GuessLauncher {
   }
 }
 
+// Класс игры
 public class GuessGame {
-
+  // Три виртуальных игрока
   Player p1, p2, p3;
 
+  // Основной рабочий метод
   public void startGame() {
+    // Создание игроков
     p1 = new Player();
     p2 = new Player();
     p3 = new Player();
 
-    int guess_p1 = 0;
-    int guess_p2 = 0;
-    int guess_p3 = 0;
-
+    // Генерирование случайного числа
     int targetNumber = (int) (Math.random() * 10);
 
+    // В цикле попытка игроками угадать число
     while(true) {
       System.out.println("Нужно угадать число: " + targetNumber);
 
+      Каждый из игроков пытается угадать число
       p1.guess();
       p2.guess();
       p3.guess();
 
+      // Вывод на экран результатов
       System.out.println("Первый игрок думает, что это число: " + p1.guess());
       System.out.println("Второй игрок думает, что это число: " + p2.guess());
       System.out.println("Третий игрок думает, что это число: " + p3.guess());
 
+      // В зависимости от того, какой из игроков выиграл, объявляется победитель
+      // и цикл прерывает свою работу. Либо игра продолжается.
       if(p1.getNumber() == targetNumber) {
         System.out.println("Первый игрок выиграл!");
         break;
@@ -44,14 +51,18 @@ public class GuessGame {
     }
   }
 
+  // Класс игрока
   public class Player {
+    // Переменная угадываемого числа
     private int number = 0;
 
+    // Метод угадывания
     public int guess() {
       number = (int) (Math.random() * 10);
       return number;
     }
 
+    // Метод получения угаданной переменной извне класса
     public int getNumber() {
       return number;
     }
