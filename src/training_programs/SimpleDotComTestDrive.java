bcf2284a9a6dd7e7be66ced34f6a5d49d7bd17d0
VadMih <mirent@mail.ru>
2019-12-18 00:21:32 +0300

    Read file SimpleDotComTestDrive.java

--- src/training_programs/SimpleDotComTestDrive.java
+++ src/training_programs/SimpleDotComTestDrive.java
@@ -1,17 +1,55 @@
 public class SimpleDotComTestDrive {
   public static void main(String [] args) {
-    SimpleDotCom dot = new SimpleDotCpm();
+    SimpleDotCom dot = new SimpleDotCom();
+    int numOfGuess = 0;
+    int startLocations = (int) (Math.random() * 8);
+    System.out.println(startLocations);
 
-    int [] locations = {2, 3, 4};
+    startLocations = 2;
+    int [] locations = {startLocations, startLocations+1, startLocations+2};
     dot.setLocationCells(locations);
 
-    String userGuess = "2";
-    String result = dot.checkYourself(userGuess);
-    String testResult = "Неудача";
-    if (result.equals("Попал") ) {
-      testResult = "Пройден";
+    while(true) {
+      String userGuess = (int) (Math.random() * 10);
+      System.out.println("Пользователь атакует ячейку: " + userGuess);
+      numOfGuess++;
+      String result = dot.checkYourself(userGuess);
+      String testResult = "Неудача";
+      if (result.equals("Потопил") ) {
+        testResult = "Пройден";
+      } else if (result.equals("Попал")) {
+        System.out.print("Счет равен: " + dot.numOfHits);
+      }
     }
 
-    System.out.println(testResult);
+    System.out.println("Потопил за " + numOfGuess + " ходов");
+  }
+}
+
+public class SimpleDotCom {
+  int [] locationCells;
+  int numOfHits = 0;
+
+  public void setLocationCells(int [] locs) {
+    locationCells = locs;
+  }
+
+  public String checkYourself(String stringGuess) {
+    int guess = Integer.parseInt(stringGuess);
+    String result = "Мимо";
+
+    for (int cell : locationCells) {
+      if (guess == cell) {
+        result = "Попал";
+        numOfHits++;
+        break;
+      }
+    }
+
+    if (numOfHits == locationCells.length) {
+      result = "Потопил";
+    }
+
+    return result;
   }
 }
