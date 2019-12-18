eaf4fb8b3c5094e14be3ca76940eee957809bf8a
VadMih <mirent@mail.ru>
2019-12-18 12:05:22 +0300

    Read file SimpleDotComTestDrive.java

--- src/training_programs/SimpleDotComTestDrive.java
+++ src/training_programs/SimpleDotComTestDrive.java
@@ -8,6 +8,7 @@ public class SimpleDotComTestDrive {
 
     int [] locations = {startLocations, startLocations+1, startLocations+2};
     dot.setLocationCells(locations);
+    dot.showLocationCells();
 
     while(true) {
       String userGuess = Integer.toString((int) (Math.random() * 10));
@@ -34,14 +35,32 @@ public class SimpleDotCom {
     locationCells = locs;
   }
 
+  public void showLocationCells() {
+    for(int i=0; i<10; i++) {
+      if(isCells(i)) System.out.print(-i);
+      else System.out.print("-0");
+    }
+    System.out.print("\n");
+  }
+
+  public boolean isCells(int i) {
+    for(int cells : locationCells) {
+      if(i == cells) return true;
+    }
+    return false;
+  }
+
   public String checkYourself(String stringGuess) {
     int guess = Integer.parseInt(stringGuess);
     String result = "Мимо";
 
-    for (int cell : locationCells) {
-      if (guess == cell) {
+    //for (int cell : locationCells) {
+    if (guess == cell && cell != -1) {
+    for(int i=0; i<10; i++) {}
+      if (guess == locationCells[i] && locationCells[i] != -1) {
         result = "Попал";
         numOfHits++;
+        locationCells[i] = -1;
         break;
       }
     }
