439f66b0ba7d99f3ca8e8689ab372e5946aaa7ac
VadMih <mirent@mail.ru>
2019-12-21 19:32:19 +0300

    Read file NavalBattle.java

--- src/training_programs/NavalBattle.java
+++ src/training_programs/NavalBattle.java
@@ -5,18 +5,64 @@ import java.io.*;
 
 // Основной класс, контролирующий работу программы
 public class NavalBattle {
-  private GameHelper gameHelper = new GameHelper();
-  private ArrayList<Ship> shipList = new ArrayList<Ship>();
-  private ArrayList<String> guessList = new ArrayList<String>();
-  private String [] literPos = {"a", "b", "c", "d", "e", "f", "g", "h"};
-  int numOfGuesses = 0;
+  private GameHelper gameHelper = new GameHelper(); // Вспомогательный класс для работы игры
+  private ArrayList<Ship> shipList = new ArrayList<Ship>(); // Список кораблей
+  private ArrayList<String> guessList = new ArrayList<String>(); // Список совершенных выстрелов
+  private ArrayList<String> createShipList = new ArrayList<String>(); //Список созданных кораблей для проверки пересечени при создании
+  private String [] literPos = {"a", "b", "c", "d", "e", "f", "g", "h"}; // Буквы координат кораблей
+  int numOfGuesses = 0; // Количество побежденных кораблей
 
+  // Начальная установка игры
   private void setUpGame() {
     // Создание трех кораблей
-    /*for(int i=0; i<10; i++) {
+    for(int i=0; i<3; i++) {
       shipList.add(new Ship(setPositionShip()));
-    }*/
-    ArrayList<String> loc = new ArrayList<String>();
+    }
+
+    // Информация начального экрана
+    System.out.println("Цель - потопить три корабля");
+    System.out.println("Постарайтель сделать это за минимальное количество ходов");
+  }
+
+  // Основной игровой цикл
+  public void startPlaying() {
+    // Перемееная игры
+    boolean game = true;
+    // Выполнять в цикле пока переменная активна
+    while(game) {
+      // Показать поле битвы
+      showBattlefield();
+      // Считать пользовательский ввод
+      String userGuess = gameHelper.getUserInput("Сделайте ход: ");
+
+      // Дополнительный автоматический игрок
+      /*String userGuess = null;
+      int startX = (int) (Math.random() * 8);
+      int startY = (int) (Math.random() * 9);
+      if(startX > 7) startX = 7;
+      if(startY > 8) startY = 8;
+      if(startY < 1) startY = 1;
+      userGuess = literPos[startX] + Integer.toString(startY);*/
+
+      // Проверить попадание, засчитать очки
+      checkUserGuess(userGuess);
+      // Если остался хоть один корабль - продолжить игру
+      for(Ship shipTest : shipList) {
+        if(shipTest.isLive()) {
+          game = true;
+          break;
+        }
+        // В противном случае прекратить
+        else game = false;
+      }
+    }
+  }
+
+// Не работает метод случайного расположения элементов
+  private ArrayList<String> setPositionShip() {
+
+    // Предустановленный набор кораблей
+    /*ArrayList<String> loc = new ArrayList<String>();
     loc.add("e2");
     loc.add("f2");
     loc.add("g2");
@@ -32,40 +78,8 @@ public class NavalBattle {
     loc2.add("c5");
     loc2.add("d5");
     loc2.add("e5");
-    shipList.add(new Ship(loc2));
+    shipList.add(new Ship(loc2));*/
 
-    // Информация начального экрана
-    System.out.println("Цель - потопить три корабля");
-    System.out.println("Постарайтель сделать это за минимальное количество ходов");
-  }
-
-  public void startPlaying() {
-    boolean game = true;
-    while(game) {
-      showBattlefield();
-      String userGuess = gameHelper.getUserInput("Сделайте ход: ");
-
-      /*String userGuess = null;
-      int startX = (int) (Math.random() * 8);
-      int startY = (int) (Math.random() * 9);
-      if(startX > 7) startX = 7;
-      if(startY > 8) startY = 8;
-      if(startY < 1) startY = 1;
-      userGuess = literPos[startX] + Integer.toString(startY);*/
-
-      checkUserGuess(userGuess);
-      for(Ship shipTest : shipList) {
-        if(shipTest.isLive()) {
-          game = true;
-          break;
-        }
-        else game = false;
-      }
-    }
-  }
-
-// Не работает метод случайного расположения элементов
-  private ArrayList<String> setPositionShip() {
     ArrayList<String> loc = new ArrayList<String>();
     ArrayList<String> locTest = new ArrayList<String>();
     int startX = (int) (Math.random() * 8);
@@ -120,17 +134,22 @@ public class NavalBattle {
     return loc;
   }
 
+  // Отобразить поле битвы
   public void showBattlefield() {
+    // Вывести верхние цифровые координаты
     System.out.print("   ");
     for(int i=1; i<9; i++) {
       System.out.print(" " + i + "  ");
     }
 
+    // Вывести боковые буквенные координаты и само поле битвы построчно
     for(int x=0; x<8; x++) {
       System.out.print("\n" + literPos[x] + " |");
       for(int y=1; y<9; y++) {
+        // Если в ячейку произведен выстрел - отметить ее
         if(guessList.indexOf(literPos[x] + Integer.toString(y)) >= 0) {
           System.out.print("[");
+          // Если в ячейке есть пораженная часть корабля - отметить ее
           if(isDeadField(literPos[x] + Integer.toString(y)))
             System.out.print("X]|");
           else System.out.print(" ]|");
@@ -140,6 +159,7 @@ public class NavalBattle {
     System.out.print("\n");
   }
 
+  // Проверка корабля на пораженные ячейки
   public boolean isDeadField(String pos) {
     for(Ship shipTest : shipList) {
       if(shipTest.getDead(pos))
@@ -148,28 +168,32 @@ public class NavalBattle {
     return false;
   }
 
+  // Основной метод проверки попадания в ячейку
   public String checkUserGuess(String userGuess) {
+    // Увеличить счетчик выстрелов
     numOfGuesses++;
+    // Задать начальное значение возвращаемого значения по умолчанию
     String result = "Мимо";
+    // Добавить этот выстрел в список выстрелов для отображения на поле битвы
     guessList.add(userGuess);
 
+    // Пройти по всем кораблям и проверить попадание по указанным координатам
     for(Ship shipTest : shipList) {
       result = shipTest.checkIsAttack(userGuess);
-
+      // В случае попадания заканчиваем цикл
       if(result.equals("Попал")) break;
-      if(result.equals("Потопил")) {
-        //shipList.remove(shipTest);
-        break;
-      }
+      if(result.equals("Потопил")) break;
     }
     return result;
   }
 
+  // Финальный метод с указанием счета
   public void finishGame() {
     System.out.println("Все корабли потоплены");
     System.out.println("Вы сделали это за " + numOfGuesses + " хода(ов)");
   }
 
+  // Основной рабочий метод
   public static void main(String [] args) {
     NavalBattle game = new NavalBattle();
     game.setUpGame();
@@ -180,31 +204,41 @@ public class NavalBattle {
 
 // Класс лодки
 public class Ship {
+  // Координаты лодки
   private ArrayList<String> locationLivePartsOfShip;
+  // Координаты побежденных ячеек лодки для отображения на общем игровом поле
   private ArrayList<String> locationDeadPartsOfShip = new ArrayList<String>();
 
   public Ship(ArrayList<String> loc) {
     this.locationLivePartsOfShip = loc;
   }
 
+  // Проверка побежденной ячейки корабля для вывода на игровом поле
   public boolean getDead(String pos) {
     if(locationDeadPartsOfShip.indexOf(pos) >= 0) return true;
     return false;
   }
 
+  // Прроверка живучести ячейки
   public boolean isLive() {
     if(locationLivePartsOfShip.isEmpty()) return false;
     else return true;
   }
 
+  // Проверка попадания по указанным координатам
   public String checkIsAttack(String userGuess) {
+    // Установка значения по умолчанию
     String result = "Мимо";
+    // Получение индекса выстрела в списке ячеек корабля
     int index = locationLivePartsOfShip.indexOf(userGuess);
 
+    // В случае попадания удалить ячейку из списка живых
+    // и добавить ее в список мертвых ячеек
     if(index >= 0) {
       locationDeadPartsOfShip.add(userGuess);
       locationLivePartsOfShip.remove(index);
 
+      // В счучае потопления корабля указать это
       if(locationLivePartsOfShip.isEmpty()) {
         System.out.println("Корабль потоплен");
         result = "Потопил";
@@ -217,6 +251,7 @@ public class Ship {
 
 // Вспомогательный класс для работы элементов игры
 public class GameHelper {
+  // Метод для ввода координат с клавиатуры
   public String getUserInput(String promt) {
     String inputLine = null;
     System.out.print(promt + " ");
