e8d2715f79dfe43c216cc5e04b4d0f1c7b5b3f8f
VadMih <mirent@mail.ru>
2020-01-05 13:59:00 +0300

    Read File

---
+++ src/training_programs/AbstractInterface.java
@@ -0,0 +1,20 @@
+//package training_programs;
+
+public class AbstractInterface {
+  public static void main(String[] args) {
+    System.out.println("132");
+  }
+}
+
+public abstract class Animals {
+  private int x;
+  private int y;
+}
+
+public class Cats extends Animals {
+
+}
+
+public class Byrd extends Animals {
+
+}
--- src/training_programs/Animal.java
+++
@@ -1,7 +0,0 @@
-//package training_programs;
-
-public class Animal {
-  public static void main(String[] args) {
-    System.out.println("132");
-  }
-}
