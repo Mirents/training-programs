96c394692cd0b3db8e49a16e8947b403c16cd76b
VadMih <mirent@mail.ru>
2020-01-06 21:21:52 +0300

    Read file

--- src/training_programs/AbstractInterface.java
+++ src/training_programs/AbstractInterface.java
@@ -1,20 +1,40 @@
 //package training_programs;
 
+import java.util.ArrayList;
+
 public class AbstractInterface {
   public static void main(String[] args) {
-    System.out.println("132");
+    ArrayList<Animals> list = new ArrayList<Animals>();
+    list.add(new Cat("Marusia"));
+    list.add(new Byrd("Vorobey"));
+
+    for(Animals a : list)
+      a.makeSound();
   }
 }
 
 public abstract class Animals {
-  private int x;
-  private int y;
+  public String name;
+
+  public abstract void makeSound();
 }
 
-public class Cats extends Animals {
+public class Cat extends Animals {
+  public Cat(String name) {
+    this.name = name;
+  }
 
+  public void makeSound() {
+    System.out.println("Cat " + name + " says Meow!");
+  }
 }
 
 public class Byrd extends Animals {
+  public Byrd(String name) {
+    this.name = name;
+  }
 
+  public void makeSound() {
+    System.out.println("Byrd " + name + " says Chik-chirik!");
+  }
 }
