96a05f684cb50905d224b1b35a07e215c531a479
VadMih <mirent@mail.ru>
2020-03-23 21:46:03 +0300

    Work!

--- src/training_programs/SimpleGUI.java
+++ src/training_programs/SimpleGUI.java
@@ -1,13 +1,25 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class SimpleGUI {
+public class SimpleGUI implements ActionListener {
+    JButton button = new JButton("Click");
+
   public static void main(String[] args) {
-      JFrame frame = new JFrame("Test");
-      JButton button = new JButton("Click");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.getContentPane().add(button);
-      frame.setSize(200, 200);
+      SimpleGUI sgui = new SimpleGUI();
+      sgui.go();
+  }
 
-      frame.setVisible(true);
+  public void go() {
+    JFrame frame = new JFrame("Test");
+    button.addActionListener(this);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(button);
+    frame.setSize(200, 200);
+
+    frame.setVisible(true);
+  }
+
+  public void actionPerformed(ActionEvent event) {
+    button.setText("Check!!!");
   }
 }
