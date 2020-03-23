2ed8e748f080bca3312711db5bbd541ca0b8c20a
VadMih <mirent@mail.ru>
2020-03-23 19:22:16 +0300

    Work to midi.

--- src/training_programs/MusicTest1.java
+++ src/training_programs/MusicTest1.java
@@ -1,1 +1,14 @@
 import java.sound.midi.*;
+
+public class MusicTest1 {
+
+  public void play() {
+    Sequencer seq = MidiSystem.getSequencer();
+    System.out.println("We get a sintez!");
+  }
+
+  public static void main(String[] args) {
+    MusicTest1 mt1 = new MusicTest1();
+    mt1.play();
+  }
+}
