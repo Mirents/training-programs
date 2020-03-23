5adcae8fe6791d7d5169951fe8a45d9935755135
VadMih <mirent@mail.ru>
2020-03-23 20:09:22 +0300

    Work!

--- src/training_programs/MusicTest1.java
+++ src/training_programs/MusicTest1.java
@@ -6,10 +6,28 @@ public class MusicTest1 {
 
   public void play() {
     try {
-      Sequencer seq = MidiSystem.getSequencer();
-      System.out.println("We get a sintez!");
-    } catch(MidiUnavailableException ex) {
-      System.out.println("Error inicialize!");
+      Sequencer player = MidiSystem.getSequencer();
+      player.open();
+
+      Sequence seq = new Sequence(Sequence.PPQ, 4);
+
+      Track track = seq.createTrack();
+
+      ShortMessage a = new ShortMessage();
+      a.setMessage(144, 1, 44, 100);
+      MidiEvent noteOn = new MidiEvent(a, 1);
+      track.add(noteOn);
+
+      ShortMessage b = new ShortMessage();
+      b.setMessage(128, 1, 44, 100);
+      MidiEvent noteOff = new MidiEvent(b, 16);
+      track.add(noteOff);
+
+      player.setSequence(seq);
+
+      player.start();
+    } catch(Exception ex) {
+      ex.printStackTrace();
     }
   }
 
