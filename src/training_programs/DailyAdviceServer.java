a98f43f08eb8ceb93c77b2f698e841e550059707
VadMih <mirent@mail.ru>
2020-04-08 20:40:30 +0300

    Work

---
+++ src/training_programs/DailyAdviceServer.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+public class DailyAdviceServer {
+  String [] adviceList = {"Ешьте меньшими порциями", "Два слова: не годится",
+  "Будьте честны хотя бы сегодня", "Смените прическу", "Прогуляйтесь", "Не стоит",
+  "Лучше полежать", "Завтра получится"};
+
+  public void go() {
+    try {
+      ServerSocket sock = new ServerSocket(4242);
+
+      PrintWriter pw = new PrintWriter(sock.getOutputStream());
+      String advice = getAdvice();
+      pw.println(advice);
+      pw.close();
+      System.out.println("Send: " + advice);
+    } catch(IOException e) { e.printStackTrace(); }
+  }
+
+  private String getAdvice() {
+    int rand = (int) (Math.random() * adviceList.length);
+    return adviceList[rand];
+  }
+
+  public static void main(String [] args) {
+    DailyAdviceServer server = new DailyAdviceServer();
+    server.go();
+  }
+}
