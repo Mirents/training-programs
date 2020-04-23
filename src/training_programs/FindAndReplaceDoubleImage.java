665d0b8d3f0a5cbd90086c58a5a91894b9552028
VadMih <mirent@mail.ru>
2020-04-23 22:24:06 +0300

    Work

--- src/training_programs/FindAndReplaceDoubleImage.java
+++ src/training_programs/FindAndReplaceDoubleImage.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 public class FindAndReplaceDoubleImage {
   JFrame frame;
   List<File> listFile = new ArrayList<File>();
+  String doubleFile = "/doubleFile";
 
   public static void main(String [] args) {
     new FindAndReplaceDoubleImage().go();
@@ -61,23 +62,35 @@ public class FindAndReplaceDoubleImage {
 
     listFile = Arrays.asList(dir.listFiles());
 
-    workToLostFile();
+    System.out.println(dir.getAbsolutePath());
+    File dirToDelete = new File(dir.getAbsolutePath() + doubleFile);
+
+    if(dirToDelete.mkdir()) // TODO добавить проверку на присутствие каталога
+      workToLostFile(dir.getAbsolutePath() + doubleFile);
+    else
+      System.out.println("Don`t create source directori!");
   }
 
-  public void workToLostFile() {
-    for(int i = 0; i < listFile.size(); i++) // TODO Сделать цикл while с начала несколько раз
+  public void workToLostFile(String source) {
+    int i = 0;
+
+    while(true) {
       for(int j = 0; j < listFile.size(); j++) {
         if(i != j && !listFile.get(i).getName().contains("_")) { // TODO добвить сравнение атрибутов и размеров файлов
           String s1 = listFile.get(i).getName().substring(0, listFile.get(i).getName().length()-4);
           String s2 = listFile.get(j).getName().substring(0, listFile.get(j).getName().length()-4);
 
           if(s1.contains(s2) || s2.contains(s1))
-            openAndDeleteFile(listFile.get(i), listFile.get(j));
+            if(openAndDeleteFile(listFile.get(i), listFile.get(j), source))
+              i = 0;
           }
+      }
+      if(i++ == listFile.size()-1)
+        break;
       }
   }
 
-  public void openAndDeleteFile(File s1, File s2) {
+  public boolean openAndDeleteFile(File s1, File s2, String source) {
     BufferedImage img1 = null;
     BufferedImage img2 = null;
     File f1 = null;
@@ -86,7 +99,6 @@ public class FindAndReplaceDoubleImage {
     try {
       img1 = ImageIO.read(s1);
       img2 = ImageIO.read(s2);
-    } catch(IOException e) { e.printStackTrace(); }
 
     int wImg1 = img1.getWidth();
     int hImg1 = img1.getHeight();
@@ -95,7 +107,21 @@ public class FindAndReplaceDoubleImage {
     int hImg2 = img2.getHeight();
     int p1 = img1.getRGB(100, 100);
     int p2 = img2.getRGB(100, 100);
-    if(p1 == p2)
-      System.out.println("Delete file " + s2.getName());
+    if(p1 == p2) {
+        //System.out.println(source + "/" + s2.getName());
+        if(s2.renameTo((new File(source + "/" + s2.getName())))) {
+          System.out.println(source + s2.getName());
+          return true;
+        }
+        else {
+          System.out.println(s2.getPath() + source + s2.getName());
+          return false;
+        }
+    } else {
+      System.out.println("33");
+      return false;
+    }
+    } catch(IOException e) { e.printStackTrace(); }
+    finally { return false; }
   }
 }
